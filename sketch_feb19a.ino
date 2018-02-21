3c99fdc59e8beb665cb670725380c6672352b6d3
ayaabdo4 <36477265+ayaabdo4@users.noreply.github.com>
2018-02-22 00:47:47 +0200

    Add files via upload

--- sketch_feb19a.ino
+++ sketch_feb19a.ino
@@ -1,48 +1,17 @@
 
-int led1=10,led2=11,led3=12;
-double r ;
-void setup() {
-  // put your setup code here, to run once:
-  pinMode (led1,OUTPUT);
-  pinMode (led2,OUTPUT);
-  pinMode (led3,OUTPUT);
-  Serial.begin(9600);
+const byte LED =10;
 
+
+void setup() 
+{
+pinMode (LED, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  r = analogRead(A2);
-  digitalWrite(led1,LOW);
-  digitalWrite(led2,LOW);
-  digitalWrite(led3,LOW);
-
-  if(r>=200&&r<400)
-  {
-  digitalWrite(led1,HIGH);
-  digitalWrite(led2,LOW);
-  digitalWrite(led3,LOW);
-
-  Serial.println(r);
-
-    }
-    if(r>=400&&r<800)
-  {
-     digitalWrite(led1,HIGH);
-  digitalWrite(led2,HIGH);
-  digitalWrite(led3,LOW);
-  Serial.println(r);
-
-    }
-    if(r>800)
-  {
-     digitalWrite(led1,HIGH);
-  digitalWrite(led2,HIGH);
-  digitalWrite(led3,HIGH);
-  Serial.println(r);
-
-    }
-  
-
+void loop()
+{
+digitalWrite (LED, HIGH );
+delay (1000);
+digitalWrite (LED, LOW );
+delay (1000);
 
 }
