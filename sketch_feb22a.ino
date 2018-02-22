505df8cacf4df9a3e63e80d28cecc2113e254128
ayahassan96 <36530128+ayahassan96@users.noreply.github.com>
2018-02-22 22:04:22 +0200

    Add files via upload
    analog.led.code

---
+++ sketch_feb22a.ino
@@ -0,0 +1,19 @@
+const byte ledPin = 3;
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ for(int n = 0 ; n <= 255 ; n++)
+ {
+  analogWrite(ledPin,n);
+  delay(10);
+  }
+  for(int n = 0 ; n >= 255 ; n--)
+  {
+    analogWrite(ledPin,n);
+    delay(10);
+    }
+}
