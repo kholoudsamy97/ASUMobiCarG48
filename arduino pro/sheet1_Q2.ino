26e42e7316d86a2f4f32b7e23c7aca1685192c24
GehadOsama <36477270+GehadOsama@users.noreply.github.com>
2018-02-22 17:58:15 +0200

    Add files via upload

---
+++ arduino pro/sheet1_Q2.ino
@@ -0,0 +1,55 @@
+const byte Led1 =4;
+const byte Led2 =5;
+const byte Led3 =6;
+const byte Led4 =7;
+
+void setup() {
+  
+pinMode (Led1,OUTPUT);
+pinMode (Led2,OUTPUT);
+pinMode (Led3,OUTPUT);
+pinMode (Led4,OUTPUT);
+
+}
+
+void loop() {
+  
+digitalWrite(Led1,HIGH);
+digitalWrite(Led2,LOW);
+digitalWrite(Led3,LOW);
+digitalWrite(Led4,LOW);
+delay (1000);
+
+digitalWrite(Led1,LOW);
+digitalWrite(Led2,HIGH);
+digitalWrite(Led3,LOW);
+digitalWrite(Led4,LOW);
+delay (1000);
+
+digitalWrite(Led1,LOW);
+digitalWrite(Led2,LOW);
+digitalWrite(Led3,HIGH);
+digitalWrite(Led4,LOW);
+delay (1000);
+
+digitalWrite(Led1,LOW);
+digitalWrite(Led2,LOW);
+digitalWrite(Led3,LOW);
+digitalWrite(Led4,HIGH);
+delay (1000);
+
+digitalWrite(Led1,LOW);
+digitalWrite(Led2,LOW);
+digitalWrite(Led3,HIGH);
+digitalWrite(Led4,LOW);
+delay (1000);
+
+digitalWrite(Led1,LOW);
+digitalWrite(Led2,HIGH);
+digitalWrite(Led3,LOW);
+digitalWrite(Led4,LOW);
+delay (1000);
+
+
+
+}
