22d289a9beb3c0f04897c46a4e5d1823c5a9aac7
Ayamohamed32 <36474364+Ayamohamed32@users.noreply.github.com>
2018-07-07 00:22:16 +0200

    Add files via upload

---
+++ Ayamohamed/infinity.ino
@@ -0,0 +1,15 @@
+Serial.println("inf");
+     
+  analogWrite(de,230);  
+  analogWrite(in,90);
+forward();
+ 
+  delay(5000);
+
+  
+ analogWrite(de,70);  
+  analogWrite(in,250);
+forward();
+  
+    delay(5000);
+    stop_motor();
