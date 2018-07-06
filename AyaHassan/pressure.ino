3848276e453a064cc74837892887e09c8925a651
ayahassan96 <36530128+ayahassan96@users.noreply.github.com>
2018-07-06 21:39:39 +0200

    Add files via upload

---
+++ AyaHassan/pressure.ino
@@ -0,0 +1,31 @@
+#include <util/delay.h>
+#include "Adafruit_BMP085.h"
+
+#include <Wire.h>
+Adafruit_BMP085 bmp;
+
+
+void setup() {
+ 
+bmp.begin();
+ Serial.begin(9600);
+}
+
+void loop() {
+
+
+//pressure
+
+Serial.print("Temperature =");
+Serial.print(bmp.readTemperature());
+Serial.println(" *C ");
+Serial.print(" Pressure = ");
+Serial.print(bmp.readPressure ());
+Serial.println(" Pa ");
+Serial.print("Altitude = ");
+Serial.print(bmp.readAltitude(101253));
+Serial.println(" Meters ");
+Serial.println();
+delay (500);
+
+}
