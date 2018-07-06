c12ef603214d684eac6712ac8775aef45a064a9f
GehadOsama <36477270+GehadOsama@users.noreply.github.com>
2018-07-06 14:59:49 +0200

    Add files via upload

---
+++ arduino pro/circle_shape_code.ino
@@ -0,0 +1,37 @@
+ char x ;
+ byte de=11,in=6;
+
+
+  void setup()
+  {
+  analogWrite(de,155);  // right_side
+  analogWrite(in,165.25);  // left_side
+    }
+
+    
+   void loop() {
+  if(x = 'q')
+  {
+    
+Serial.println("circ");
+analogWrite(de,96);  
+analogWrite(in,232);
+forward();
+delay(3997);
+analogWrite(de,90);  
+analogWrite(in,232);
+forward();    
+delay(3997);
+
+
+stop_motor();
+    }
+    
+      
+    
+    
+    
+    
+    }
+
+  
